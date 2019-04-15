8ca6030b29ef6f3559c9c40dd90891848e3e6b12
Zsuzsi Varga <zsuzsivarga@Zsuzsis-MacBook-Pro.local>
2019-04-15 09:21:38 +0100

    Frontend dev branch for Susie created

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+    FireBaseConfig: {
+        apiKey: "AIzaSyCJqVuUvDjLCsCbRxMebMqvc6pNe7eAi2Y",
+        authDomain: "react-native-dev-f4b63.firebaseapp.com",
+        databaseURL: "https://react-native-dev-f4b63.firebaseio.com",
+        projectId: "react-native-dev-f4b63",
+        storageBucket: "react-native-dev-f4b63.appspot.com",
+        messagingSenderId: "709280467693"
+    }
+}
