45e64225ca8a3f5ffa25e2cc81347d55af904d9a
Zsuzsi Varga <zsuzsivarga@zsuzsis-mbp.home>
2019-04-22 22:13:05 +0100

    Single View design ready

--- final-project-dev/screens/SingleView.js
+++ final-project-dev/screens/SingleView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, WebView } from "react-native";
-// import { Button, Content, Input, Item, Icon } from "native-base";
+import { View, Text, StyleSheet, WebView, Image, ImageBackground } from "react-native";
+import { Container, Header, Content, Card, CardItem, Body, Icon, Button, Left, Right } from "native-base";
 import axios from 'axios';
 
 export default class SingleView extends Component {
@@ -17,17 +17,134 @@ export default class SingleView extends Component {
     render() {
         const { navigation } = this.props;
         const venue = navigation.getParam('venue', 'NO-NAME');
-        return (<View>
-            <Text>{venue.name}</Text>
-            <Text>{venue.description}</Text>
-            <Button onPress={() => navigation.goBack()} title="Back"></Button>
-            <Button onPress={() => this._patchId(venue.restaurant_id)} title="360 view"></Button>
-        </View>
+        return (
+            <View style={styles.page}>
+
+                {/* <Button style={styles.backlink} onPress={() => navigation.goBack()} title="Back"></Button> */}
+                <ImageBackground source={{ uri: venue.photo_url }} style={styles.image}  >
+                    <Icon name='arrow-back' style={styles.back} onPress={() => navigation.goBack()} />
+                </ImageBackground>
+                <Content padder>
+                    <Card transparent>
+                        <CardItem header style={styles.header}>
+                            <Left>
+                                <Text style={styles.name}>{venue.name}</Text>
+                            </Left>
+                            <Right>
+                                <Icon active name='eye' title="360 view" style={styles.icon} onPress={() => this._patchId(venue.restaurant_id)}></Icon>
+                                {/* <Button iconLeft style={styles.button} onPress={() => this._patchId(venue.restaurant_id)} title="360 view"> */}
+                                {/* <Icon name='eye' /> */}
+                                {/* <Text style={styles.text}>360 View</Text> */}
+                                {/* </Button> */}
+                            </Right>
+                        </CardItem>
+                        <CardItem style={styles.container}>
+                            <View style={styles.line}></View>
+                            <Text style={styles.description}>Description</Text>
+                            <View style={styles.line}></View>
+                        </CardItem>
+                        <CardItem>
+                            <Body>
+                                <Text style={styles.overview}>{venue.description}</Text>
+                            </Body>
+                        </CardItem>
+                    </Card>
+                </Content>
+                <View style={styles.footer}>
+                    <View style={styles.stars}>
+                        {Array.apply(null, { length: venue.rating }).map((e, i) => (
+                            <Icon active name='star' style={styles.iconStar} key={i}></Icon>
+                        ))}
+                    </View>
+                </View>
+            </View>
 
         );
     }
 }
 
 const styles = StyleSheet.create({
+    image: {
+        height: 350,
+        width: '100%',
+        resizeMode: 'cover'
+    },
+    page: {
+        width: '100%',
+        height: '100%'
+    },
+    name: {
+        color: 'black',
+        fontSize: 24,
+        fontFamily: 'Copperplate-Light',
+        paddingLeft: 0,
+        paddingRight: 0,
+        paddingTop: 0,
+        paddingBottom: 0
+    },
+    button: {
+        width: '70%',
+        marginRight: '5%',
+        marginLeft: '5%',
+        height: 48,
+        margin: 10,
+        color: '#D9D9D9'
+    },
+    text: {
+        color: "white",
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 36,
+        color: '#D9D9D9'
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: 0
 
+    },
+    line: {
+        backgroundColor: '#D9D9D9',
+        height: 1.5,
+        width: '35%'
+    },
+
+    description: {
+        fontSize: 14,
+        color: 'black'
+    },
+    overview: {
+        fontSize: 14,
+        color: '#B9B9B9',
+        textAlign: 'justify'
+    },
+    back: {
+        position: 'absolute',
+        top: 30,
+        left: 30,
+        height: 30,
+        width: 30,
+        color: 'white'
+    },
+    footer: {
+        backgroundColor: '#D9D9D9',
+        paddingBottom: 0,
+        width: '100%',
+        height: 50
+    },
+    iconStar: {
+        fontSize: 14,
+        color: 'white',
+        margin: 2,
+
+    },
+    stars: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginLeft: 20
+
+    },
 });
